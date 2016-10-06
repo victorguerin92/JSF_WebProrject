27af897123810f10d43129ef934ab0fb52411865
inti0227 <inti0227@DESKTOP-S9V0O4L.home>
2016-10-06 11:23:47 +0200

    mon troisieme commit

--- 09_JSF_WebProject/src/fr/adaming/managedBeans/MonBeansAgent.java
+++ 09_JSF_WebProject/src/fr/adaming/managedBeans/MonBeansAgent.java
@@ -2,11 +2,29 @@ package fr.adaming.managedBeans;
 
 import java.io.Serializable;
 
+import fr.adaming.model.Agent;
+
 public class MonBeansAgent implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private Agent a;
+	
 
+	/**
+	 * @return the a
+	 */
+	public Agent getA() {
+		return a;
+	}
+
+	/**
+	 * @param a the a to set
+	 */
+	public void setA(Agent a) {
+		this.a = a;
+	}
 }
